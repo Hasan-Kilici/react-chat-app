495b4050b278d72b8dedb64969c44c4eedc8d7d9
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-11-04 02:14:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const reactViews = require('express-react-views');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use('/public', express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine());
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+app.get('/', (req, res) => {
+	res.render('index');
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
